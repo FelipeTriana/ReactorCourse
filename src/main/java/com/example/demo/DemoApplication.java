803b626d8e1eee1c4b219550aaadc31d8f57b2ae
Felipe Triana <felipetrianago@gmail.com>
2024-09-11 23:00:58 -0500

    Se añade ejemplo de transformacion de un lista con .fromIterable

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,8 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
-//Imutabilidad: Cada vez que aplicamos un operador a un flujo se van creando nuevas instancias pero nunca se modifica el flujo original
-//Lo anterior se puede evidenciar en el ejemplo a continuacion, donde se aplica el operador map y filter a un flujo de nombres
+import java.util.ArrayList;
+import java.util.List;
+
+//Si por ejemplo realizamos una consulta a la base de datos obtenemos una lista de objetos, podemos convertir ese listado en un flujo de datos con el metodo fromIterable
+//Hay bases de datos NoSql que son reactivas y ya devuelven un flujo de datos en vez de una lista, por ejemplo MongoDB
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
@@ -21,8 +24,14 @@ public class DemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		//El operador filter es para filtrar los elementos que cumplan con la condicion dada, evalúa una condicion BOOLEANA
-		Flux<String> nombres = Flux.just("Andres Guzman","Pedro Anuar","Diego Ciruela","Zazza Kepaza","Juan Planeta");
+		List<String> usuariosList = new ArrayList<>();
+		usuariosList.add("Andres Guzman");
+		usuariosList.add("Pedro Anuar");
+		usuariosList.add("Diego Ciruela");
+		usuariosList.add("Zazza Kepaza");
+		usuariosList.add("Juan Planeta");
+		//fromIterable genera un flujo a apartir de un objeto iterable
+		Flux<String> nombres = Flux.fromIterable(usuariosList);//Flux.just("Andres Guzman","Pedro Anuar","Diego Ciruela","Zazza Kepaza","Juan Planeta");
 
 		Flux<Usuario> usuarios = nombres
 				.map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
