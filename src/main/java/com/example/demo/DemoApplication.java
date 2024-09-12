442afb9688e18a317868f2df89572a0a72a6dc75
Felipe Triana <felipetrianago@gmail.com>
2024-09-11 22:27:14 -0500

    Se explica y se añade ejemplo que incluye el operador filter y evidencia la inmutabilidad de los Observables

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
-
+//Imutabilidad: Cada vez que aplicamos un operador a un flujo se van creando nuevas instancias pero nunca se modifica el flujo original
+//Lo anterior se puede evidenciar en el ejemplo a continuacion, donde se aplica el operador map y filter a un flujo de nombres
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
@@ -20,15 +21,17 @@ public class DemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		//El operador map es un operador de transformacion, es decir, se encarga de transformar los elementos de un flujo de datos.
-		//Map siempre debe retornar algo
-		Flux<Usuario> nombres = Flux.just("Andres","Pedro","Diego","Zazza","Juan")
-				.map(nombre -> new Usuario(nombre.toUpperCase(), null))
+		//El operador filter es para filtrar los elementos que cumplan con la condicion dada, evalúa una condicion BOOLEANA
+		Flux<String> nombres = Flux.just("Andres Guzman","Pedro Anuar","Diego Ciruela","Zazza Kepaza","Juan Planeta");
+
+		Flux<Usuario> usuarios = nombres
+				.map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
+				.filter(usuario -> usuario.getNombre().equalsIgnoreCase("zazza"))
 				.doOnNext(e -> {
 					if (e == null){
 						throw new RuntimeException("Nombres no pueden ser vacios");
 					}
-					System.out.println(e.getNombre());
+					System.out.println(e.getNombre() + " " + e.getApellido());
 				})
 				.map(usuario -> {
 					String nombre = usuario.getNombre().toLowerCase();
@@ -37,6 +40,15 @@ public class DemoApplication implements CommandLineRunner {
                 });
 
 		nombres.subscribe(e -> log.info(e.toString()),
+				error -> log.error(error.getMessage()),
+				new Runnable() {
+					@Override
+					public void run() {
+						log.info("Ha finalizado la ejecucion del observable con exito");
+					}
+				});
+
+		usuarios.subscribe(e -> log.info(e.toString()),
 				error -> log.error(error.getMessage()),
 				new Runnable() {
 					@Override
